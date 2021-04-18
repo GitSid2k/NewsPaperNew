dcc194b06d93a837a62d951ec79b2bc34b93c94d
GitSid2k <sidorov2a@gmail.com>
2021-04-18 12:09:41 +0300

    NewsTable

--- news/models.py
+++ news/models.py
@@ -1,6 +1,6 @@
-from django.db import models
 from django.contrib.auth.models import User
-
+from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class Author(models.Model):

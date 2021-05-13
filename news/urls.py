31a5ea9cf4b4404124ac02bb0fa60da846bea730
GitSid2k <sidorov2a@gmail.com>
2021-05-13 15:17:35 +0300

    Generic

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import PostList, PostCreateView, PostDetailView
+from .views import PostList, PostCreateView, PostDetailView, PostDeleteView, PostUpdateView
 
 from django.conf.urls import url
 from django_filters.views import object_filter
@@ -11,5 +11,8 @@ urlpatterns = [
     # path('<int:pk>', NewsDetail.as_view()),
     url(r'^list/$', object_filter, {'model': Post}),
     path('<int:pk>/', PostDetailView.as_view(), name='post_detail'),
-    path('create/', PostCreateView.as_view(), name='post_create'),
+    path('post_create/', PostCreateView.as_view(), name='post_create'),
+    path('post_delete/', PostDeleteView.as_view(), name='post_delete'),
+    path('post_update/', PostUpdateView.as_view(), name='post_update'),
+
 ]
